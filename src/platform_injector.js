f224118a6e833c75e0430d3e5699e45b8c7a83a1
Will Ballard <wballard@mailframe.net>
2014-06-24 11:42:12 -0500

    conditional polymer injection

--- src/platform_injector.js
+++ src/platform_injector.js
@@ -1,6 +1,21 @@
 //bootstrap polymer platform shims if not detected
 
 if (typeof Platform === "undefined") {
+  //trap header access
+  var real_getResponseHeader = XMLHttpRequest.prototype.getResponseHeader;
+  XMLHttpRequest.prototype.getResponseHeader = function(header) {
+    try {
+      var headers = this.getAllResponseHeaders();
+      if (headers.indexOf("Location") >= 0) {
+        return real_getResponseHeader.call(this, header);
+      } else {
+        return undefined;
+      }
+    } catch (e) {
+      return undefined;
+    }
+  }
+
   console.log('Polymer injecting');
   var injector = document.querySelector('script[platform]');
   var platform = document.createElement('script');
